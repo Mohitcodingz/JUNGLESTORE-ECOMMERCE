526087e7c1abc983e5b0d3b4344b15d46730466f
mohitcodingz <mohitsingh2003uk@gmail.com>
2026-08-08 01:45:57 +0530

    finalgoodnight

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,8 +1,37 @@
-const registerUser = (req, res) => {
-    console.log(req.body);
-    res.json({
-        msg: "This function is working properly"
-    })
+const UserModel = require('../models/user');
+
+const registerUser = async (req, res) => {
+    const existingUser = await UserModel.findOne({
+        email: req.body.email,
+        name: req.body.email
+    }
+    )
+    if (existingUser) {
+        return res.json({
+            success: false,
+            message: "User Already Registered"
+        })
+    }
+    else {
+
+        try {
+            const newUser = new UserModel(req.body);
+            await newUser.save();
+            res.status(200).json({
+                success: true,
+                message: 'User Registered Successfully'
+            })
+        }
+        catch (err) {
+            console.error(err);
+
+            res.status(500).json({
+                success: false,
+                message: "User Registered Failed"
+            });
+        }
+    }
+
 }
 module.exports = {
     registerUser
