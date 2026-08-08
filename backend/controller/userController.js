71a285c058dcd1816a6daa7931fccaef07af8bd9
mohitcodingz <mohitsingh2003uk@gmail.com>
2026-08-08 22:19:39 +0530

    process1

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,38 +1,52 @@
-const UserModel = require('../models/user');
-
-const registerUser = async (req, res) => {
-    const existingUser = await UserModel.findOne({
+const user = require('../models/user')
+const bcrypt = require('bcrypt')
+async function registerUser(req, res) {
+    const existingUser = await user.findOne({
         email: req.body.email,
-        name: req.body.email
-    }
-    )
+    })
     if (existingUser) {
+        // console.log('The email is already regsitered!')
         return res.json({
-            success: false,
-            message: "User Already Registered"
+            msg: "The Email is already Registered",
+            success: false
         })
     }
     else {
-
         try {
-            const newUser = new UserModel(req.body);
+
+            let password = req.body.password;
+            password = bcrypt.hashSync(password, 8);
+            const userData = {
+                name: req.body.name,
+                email: req.body.email,
+                password: password
+            }
+            const newUser = new user(userData);
             await newUser.save();
-            res.status(200).json({
-                success: true,
-                message: 'User Registered Successfully'
+            console.log('The Register Details are saved Successfully!')
+            res.status(201).json({
+                msg: "The registration is done successfully!",
+                success: true
             })
         }
-        catch (err) {
-            console.error(err);
-
+        catch (error) {
+            console.error(error);
             res.status(500).json({
-                success: false,
-                message: "User Registered Failed"
-            });
+                msg: "The registration Failed.",
+                success: false
+            })
         }
     }
+}
+async function loginUser(req,res) {
+let email = req.body.email;
+let password =  req.body.password;
+const userExists = await user.findOne({
+    email:req.body.email
+ })
+ if(userExists){
 
+ }
 }
-module.exports = {
-    registerUser
-}
+
+module.exports = { registerUser, loginUser }
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { registerUser} = require('./../controller/userController')
-router.post('/register',registerUser)
+const { registerUser, loginUser} = require('./../controller/userController')
+router.post('/register',registerUser);
+router.post('/login',loginUser)
 module.exports = router;
