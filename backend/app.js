0dfde042bc84154e49466c030af505cc5dabc940
mohitcodingz <mohitsingh2003uk@gmail.com>
2026-08-13 21:47:51 +0530

    done

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors')
 const userRoutes = require('./routes/userRoutes');
-const profileRoutes = require('./routes/profileRoutes');
+const profileRoutes  = require('./routes/profileRoutes');
 const app = express();
 app.use(express.json());
 app.use(cors());
